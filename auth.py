55cc1c9ec836350fbeb5b48979a7bde6f093de5f
Alejandro Ramirez <alejandroramirez@alejandros-mbp.lan>
2021-03-21 16:55:32 -0700

    Testing changes to auth

---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key        = 'fPmQKbfFjXJE5OwbCSbouwplU'
+consumer_secret     = 'NpdO7WZGCPjAj6RsGmWJuaK3IM1f7hWjgRJGm4B4MDupwKWKUp'
+access_token_key    = '1373507818972532736-EQmDVgUwQlKPcDJp05FBPhf4DQi55d'
+access_token_secret = 'wkqIdAeXsWnjWAHR9h2l16GgQoebULAKTLRcxPA5whJ6t'
--- twitterBot.py
+++ twitterBot.py
@@ -2,16 +2,16 @@ from TwitterAPI import TwitterAPI
 import os
 from os import environ
 
-#from auth import (
-#    consumer_key,
-#    consumer_secret,
-#    access_token_key,
-#    access_token_secret
-#)
-consumer_key = environ['CONSUMER_KEY']
-consumer_secret = environ['CONSUMER_SECRET']
-access_token_key = environ['ACCESS_KEY']
-access_token_secret = environ['ACCESS_SECRET']
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token_key,
+    access_token_secret
+)
+#consumer_key = environ['CONSUMER_KEY']
+#consumer_secret = environ['CONSUMER_SECRET']
+#access_token_key = environ['ACCESS_KEY']
+#access_token_secret = environ['ACCESS_SECRET']
 
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
