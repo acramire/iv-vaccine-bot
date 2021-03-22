50fe2a5cb8ba14b7f4bcf837cd754230c1f216b1
Alejandro Ramirez <alejandroramirez@alejandros-mbp.lan>
2021-03-21 17:03:14 -0700

    Removed runtime, changed back auth in twitter bot

--- imperialBot.py
+++ imperialBot.py
@@ -8,12 +8,12 @@ import twitterBot
 import os
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+#chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 #PATH = "/Users/alejandroramirez/Desktop/chromeDriverSpot/chromedriver"
 #driver = webdriver.Chrome(PATH)
